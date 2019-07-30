55cd09bd5b1d182b642c97bb23e064ab5db71887
VietThanAccre <thanhoangviet@gmail.com>
2019-07-30 09:59:47 -0500

    found good parameters for Matlab AdaptThresh

--- MatAdaptThresh/AdaptThresh.m
+++ MatAdaptThresh/AdaptThresh.m
@@ -1,5 +1,6 @@
 I = niftiread('volume_norm_tif_Hessian_0p8_1p0_250p0_2p0_11p0_10.nii');
-A = adaptthresh(I, 0.001, 'NeighborhoodSize', [5 5 5]);
-B = rescale(A, 0, 255);
-B = single(B);
-niftiwrite(B, 'out.nii');
+% has to be odd 
+T = adaptthresh(I, 0.00001, 'NeighborhoodSize', [3 3 3]);
+BW = imbinarize(I, T);
+BW = single(BW);
+niftiwrite(BW, 'outBinarize_0p00001_3.nii');
