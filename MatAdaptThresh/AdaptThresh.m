2d1fe5139ea845fbb7549c99cb901219e6c74082
VietThanAccre <thanhoangviet@gmail.com>
2019-08-01 16:51:24 -0500

    A small commit, probably README

--- MatAdaptThresh/AdaptThresh.m
+++ MatAdaptThresh/AdaptThresh.m
@@ -1,6 +1,19 @@
-I = niftiread('volume_norm_tif_Hessian_0p8_1p0_250p0_2p0_11p0_10.nii');
+name = sprintf('volume_norm_tif_Hessian_0p001_1p0_1p0_2p0_6p0_9');
+inputName = append(name, sprintf('.nii'));
+outputName = append(sprintf('Adapt5_'), name, sprintf('.nii'));
+disp(['Input: ' inputName]);
+disp(['Output: ' outputName]);
+
+I = niftiread(inputName);
+disp('File read');
+
 % has to be odd 
-T = adaptthresh(I, 0.001, 'NeighborhoodSize', [1 1 1]);
+T = adaptthresh(I, 0.01, 'NeighborhoodSize', [5 5 5]);
+disp('Threshold evaluated');
+
 BW = imbinarize(I, T);
 BW = single(BW);
-niftiwrite(BW, 'outBinarize_0p001_1.nii');
+disp('Binarized');
+
+niftiwrite(BW, outputName);
+disp('File written out');
